aa6e978dd8dbf305272e4a557d598c59e8f3136b
Dario <dariogaleano.17@gmail.com>
2022-06-09 04:29:34 -0400

    style(#2): add saas and prepare project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
