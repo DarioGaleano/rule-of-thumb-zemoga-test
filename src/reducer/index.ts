c4d5155970394a7a08cdadd1788a4aa6b5374456
Dario <dariogaleano.17@gmail.com>
2022-06-11 17:57:09 -0400

    refact(#3): delete unused actions

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,17 +1,31 @@
-import { CardWithId } from '../types/types';
+import { Celebs } from '../types/types';
 import { actions } from '../config/actions';
 
-export const celebsReducer = (state: CardWithId[], action: { type: string; payload: any }) => {
+export const celebsReducer = (state: Celebs[], action: { type: string; payload: any }) => {
 	switch (action.type) {
-		case actions.add:
-			return [...state, action.payload];
 		case actions.toggleUp:
 			return state.map((celeb) =>
-				celeb._id === action.payload ? { ...celeb, votes: { positive: celeb.votes.positive + 1, negative: celeb.votes.negative } } : celeb
+				celeb._id === action.payload
+					? {
+							...celeb,
+							votes: {
+								positive: celeb.votes.positive + 1,
+								negative: celeb.votes.negative,
+							},
+					  }
+					: celeb
 			);
 		case actions.toggleDown:
 			return state.map((celeb) =>
-				celeb._id === action.payload ? { ...celeb, votes: { positive: celeb.votes.positive, negative: celeb.votes.negative + 1 } } : celeb
+				celeb._id === action.payload
+					? {
+							...celeb,
+							votes: {
+								positive: celeb.votes.positive,
+								negative: celeb.votes.negative + 1,
+							},
+					  }
+					: celeb
 			);
 		default:
 			return state;
