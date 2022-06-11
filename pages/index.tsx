7c26202be23e3b0d70d0bc366a903af428076925
Dario <dariogaleano.17@gmail.com>
2022-06-10 21:07:37 -0400

    feat(#3): add Banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next';
 import { NavBar } from '../src/components/NavBar';
 import { Header } from '../src/components/Header';
+import { Banner } from '../src/components/Banner';
 
 const Home: NextPage = () => {
 	return (
 		<div>
 			<NavBar />
 			<Header />
+			<div className='max-centered'>
+				<Banner />
+			</div>
 		</div>
 	);
 };
